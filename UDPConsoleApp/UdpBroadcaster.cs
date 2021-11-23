5779bfbd2537d45a32a536ba51a1fd5086c76fd6
Sherifsss <kristaps_mezatucs2@inbox.lv>
2021-11-23 13:50:34 +0100

    Add project files.

---
+++ UDPConsoleApp/UdpBroadcaster.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPConsoleApp
+{
+    public class UdpBroadcaster
+    {
+        public static void SendMessage(string message)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            System.Net.Sockets.UdpClient socket = new System.Net.Sockets.UdpClient();
+            socket.Send(data, data.Length, "255.255.255.255", 10010);
+            while (true)
+            {
+                IPEndPoint from = null;
+                byte[] recieveData = socket.Receive(ref from);
+                string recievedString = Encoding.UTF8.GetString(recieveData);
+                Console.WriteLine(recievedString);
+            }
+        }
+    }
+}
---
+++ UDPConsoleApp/UdpListener.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPConsoleApp
+{
+    public class UdpListener
+    {
+        public static void StartListener()
+        {
+            UdpClient socket = new UdpClient();
+            socket.Client.Bind(new IPEndPoint(IPAddress.Any, 10010));
+
+            while (true)
+            {
+                IPEndPoint from = null;
+                byte[] data = socket.Receive(ref from);
+                string received = Encoding.UTF8.GetString(data);
+                Console.WriteLine($"{received}");
+            }
+
+
+        }
+    }
+}
